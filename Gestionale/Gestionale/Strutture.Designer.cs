eaf08313e79a6a6a9276de846943406d9aafffda
Daniele <daniele.venturin@barsanti.edu.it>
2021-02-15 21:04:53 +0100

    Aggiunto sistema per inserimento strutture

--- Gestionale/Gestionale/Strutture.Designer.cs
+++ Gestionale/Gestionale/Strutture.Designer.cs
@@ -26,23 +26,142 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIndirizzo = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMassimali = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.datiPazienti = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datiPazienti)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtIndirizzo);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtMassimali);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 126);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aggiungi Struttura";
+            // 
+            // txtIndirizzo
+            // 
+            this.txtIndirizzo.Location = new System.Drawing.Point(6, 50);
+            this.txtIndirizzo.Name = "txtIndirizzo";
+            this.txtIndirizzo.Size = new System.Drawing.Size(100, 22);
+            this.txtIndirizzo.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(27, 28);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Indirizzo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(233, 28);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Massimali";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(185)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(90, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Aggiungi Struttura";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMassimali
+            // 
+            this.txtMassimali.Location = new System.Drawing.Point(218, 50);
+            this.txtMassimali.Name = "txtMassimali";
+            this.txtMassimali.Size = new System.Drawing.Size(100, 22);
+            this.txtMassimali.TabIndex = 9;
+            this.txtMassimali.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLuogoDN_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(138, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "E-Mail";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(112, 50);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // datiPazienti
+            // 
+            this.datiPazienti.AllowUserToAddRows = false;
+            this.datiPazienti.AllowUserToDeleteRows = false;
+            this.datiPazienti.AllowUserToResizeRows = false;
+            this.datiPazienti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datiPazienti.Location = new System.Drawing.Point(12, 144);
+            this.datiPazienti.Name = "datiPazienti";
+            this.datiPazienti.ReadOnly = true;
+            this.datiPazienti.RowHeadersWidth = 51;
+            this.datiPazienti.RowTemplate.Height = 24;
+            this.datiPazienti.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.datiPazienti.Size = new System.Drawing.Size(328, 377);
+            this.datiPazienti.TabIndex = 11;
             // 
             // Strutture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 533);
+            this.ClientSize = new System.Drawing.Size(699, 533);
+            this.Controls.Add(this.datiPazienti);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Strutture";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Strutture";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datiPazienti)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtIndirizzo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtMassimali;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.DataGridView datiPazienti;
     }
 }
--- Gestionale/Gestionale/Strutture.cs
+++ Gestionale/Gestionale/Strutture.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,80 @@ namespace Gestionale
 {
     public partial class Strutture : Form
     {
+        database db = new database();
+        public string[] valori = new string[36];
         public Strutture() {
             InitializeComponent();
+            carica();
+            stampaLista();
+        }
+
+        public void carica() {
+            for (int i = 0; i < 36; i++)
+            {
+                if (i < 10)
+                {
+                    valori[i] = Convert.ToString(i);
+                }
+                else
+                {
+                    valori[i] = Convert.ToString((char)('a' + i - 10));
+                }
+            }
+        }
+
+        public string UUID() {
+            string cu = "";
+            Random rnd = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                if (cu.Length == 3 || cu.Length == 12)
+                {
+                    cu += "-";
+                }
+                else
+                {
+                    cu += valori[rnd.Next(0, 36)];
+                }
+            }
+            return cu.ToUpper();
+        }
+        private void stampaLista() {
+            string comandosql = "SELECT ids,massimali,indirizzo FROM strutture";
+            using (SQLiteConnection connessione = new SQLiteConnection(db.stringaConnessione))
+            {
+                connessione.Open();
+                using (SQLiteCommand comando = new SQLiteCommand(comandosql, connessione))
+                {
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(comando);
+                    DataSet ds = new DataSet("tabelle");
+                    da.Fill(ds, "tabella");
+                    datiPazienti.DataSource = ds.Tables["tabella"];
+                    datiPazienti.Refresh();
+                }
+                connessione.Close();
+            }
+        }
+
+        private void txtLuogoDN_KeyPress(object sender, KeyPressEventArgs e) {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (txtEmail.Text != "" && txtIndirizzo.Text != "" && txtMassimali.Text != "") {
+                db.esegui(string.Format("INSERT INTO strutture(ids, indirizzo, massimali, email) VALUES('{0}', '{1}', {2}, '{3}')", UUID(), txtIndirizzo.Text, Convert.ToInt32(txtMassimali.Text), txtEmail.Text));
+                stampaLista();
+            }
         }
     }
 }
